49947fb2aba21195f05728c6522ce1ef52270682
AlexBlacksmith <unbloody.alex@gmail.com>
2022-03-05 18:39:08 +0500

    Added CSS

--- index.php
+++ index.php
@@ -39,24 +39,27 @@ if($num < 0 || $num > 100){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <title>Угадай число</title>
     </head>
     <body>
-        <h1>Угадай число</h1>
-        <p>Привет, мы загадали число в диапазоне от 1 до 100. Попробуй угадай)))</p>
-        <form action="index.php" method="POST">
-            <input type="number" name="num" placeholder="Введите число">
-            <input type="submit" name="submit" value="Проверить">
-            <p><input type="submit" name="other" value="Другое число"></p>
-        </form>
-        <hr>
-            <?php
-             echo $_SESSION['false']; unset($_SESSION['false']);
-             echo $_SESSION['error']; unset($_SESSION['error']);
-             echo $_SESSION['small']; unset($_SESSION['small']);
-             echo $_SESSION['big']; unset($_SESSION['big']);
-             echo $_SESSION['true']; unset($_SESSION['true']);           
-            ?>
-        <hr>
+        <div class="container">
+            <h1>Угадай число</h1>
+            <p>Привет, мы загадали число в диапазоне от 1 до 100. Попробуй угадай)))</p>
+            <form action="index.php" method="POST">
+                <input type="number" name="num" placeholder="Введите число">
+                <input type="submit" name="submit" value="Проверить">
+                <p><input type="submit" name="other" value="Другое число"></p>
+            </form>
+            <hr>
+                <?php
+                echo $_SESSION['false']; unset($_SESSION['false']);
+                echo $_SESSION['error']; unset($_SESSION['error']);
+                echo $_SESSION['small']; unset($_SESSION['small']);
+                echo $_SESSION['big']; unset($_SESSION['big']);
+                echo $_SESSION['true']; unset($_SESSION['true']);           
+                ?>
+            <hr>
+        </div>
     </body>
 </html>
